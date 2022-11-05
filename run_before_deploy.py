0706801d247858bc18b90afa2e907994e5b4a064
Jakub Dakowski <jakubdakowski@gmail.com>
2022-11-05 23:58:24 +0100

    new icons, changed color scheme, removed pico files

--- run_before_deploy.py
+++ run_before_deploy.py
@@ -1,11 +1,15 @@
 from favicons import Favicons
+from cdnget import Unpkg
 
 # Generating favicons
 
 _YOUR_ICON = "raw_files/favicon.png"
 _WEB_SERVER_ROOT = "content/magics"
+_PICO_NAME = "@picocss/pico"
 
 with Favicons(_YOUR_ICON, _WEB_SERVER_ROOT) as favicons:
     favicons.generate()
     FAVI_NAMES = favicons.filenames()
     FAVI_HTML = "\n".join(favicons.html())
+    
+Unpkg().download(_PICO_NAME, Unpkg().latest_version(_PICO_NAME), "theme/static/css", quiet=True)
