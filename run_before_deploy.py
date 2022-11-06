e2050eca981467aaa2d6a0207d7f5fa1e45c1e32
Jakub Dakowski <jakubdakowski@gmail.com>
2022-11-06 17:25:57 +0100

    migrate images, improve template setup

--- run_before_deploy.py
+++ run_before_deploy.py
@@ -1,16 +1,19 @@
 from favicons import Favicons
 from cdnget import Unpkg
+import os
 
 # Generating favicons
 
 _YOUR_ICON = "raw_files/favicon.png"
 _WEB_SERVER_ROOT = "content/magics"
-_PICO_NAME = "@picocss/pico"
 
 print("Generating favicons")
 with Favicons(_YOUR_ICON, _WEB_SERVER_ROOT) as favicons:
     favicons.generate()
     FAVI_NAMES = favicons.filenames()
     FAVI_HTML = "\n".join(favicons.html())
-    
-Unpkg().download(_PICO_NAME, Unpkg().latest_version(_PICO_NAME), "theme/static/css")
+
+if all(i != "@picocss" for i in os.listdir("theme/static/css")):
+    Unpkg().download("@picocss/pico", "1.5.6", "theme/static/css")
+else:
+    print("Found pico.css")
